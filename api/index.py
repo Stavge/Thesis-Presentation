4b53016f6cdcb934f307eb04113dec4e29f64fc2
Stavge <georgoulastavrina@gmail.com>
2025-05-10 18:49:49 +0300

    Test minimal Flask app

--- api/index.py
+++ api/index.py
@@ -2,6 +2,6 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/scenario/<num>')
-def get_scenario(num):
-    return jsonify({"test": "data"})
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello from Flask on Vercel!"})
